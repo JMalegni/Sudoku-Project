131d2869ed0967341f089dcc3f233159a64b4f89
pgutierrez1 <113554996+pgutierrez1@users.noreply.github.com>
2022-11-20 20:36:39 -0600

    wrote test case for SudokuGenerator.get_board() to see the state of board

---
+++ tests/test_sudoku_generator.py
@@ -0,0 +1,16 @@
+import unittest
+from sudoku_generator import SudokuGenerator
+
+class SudokuGeneratorTest(unittest.TestCase):
+
+    def test_get_board(self):
+        # currently testing printed output
+        print("Empty board:")
+        # board is printed when get_board is called inside SudokuGenerator.__init__()
+        gen = SudokuGenerator(9, 0)
+        self.assertEqual(None, None)
+
+if __name__ == "__main__":
+    # Allows for print() statements to be displayed in console output
+    runner = unittest.TextTestRunner(verbosity=2)
+    unittest.main(testRunner=runner)
